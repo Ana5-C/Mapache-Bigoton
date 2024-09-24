24d42a1f11829e946954b5de9fcd582b7b15cca7
jose-Antonio0270 <159972295+jose-Antonio0270@users.noreply.github.com>
2024-09-24 10:57:43 -0600

    Update InicioSesionComponent.js

--- mapachebigoton/src/components/InicioSesionComponent.js
+++ mapachebigoton/src/components/InicioSesionComponent.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import React, { useState } from 'react';
 import ClienteService from '../services/ClienteService';
 import { Link } from 'react-router-dom';
 
