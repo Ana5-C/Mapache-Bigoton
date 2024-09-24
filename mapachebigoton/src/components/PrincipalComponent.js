fc59729a01ae0965ef5a59ff0df4290cd3c8e237
Belen Hernandez <mary13hernandez2003@gmail.com>
2024-09-23 22:09:48 -0600

    commit actualizacion
    nuevo diseño de la pagina principal

--- mapachebigoton/src/components/PrincipalComponent.js
+++ mapachebigoton/src/components/PrincipalComponent.js
@@ -1,31 +1,32 @@
-import React, { useState } from "react";
-import PrincipalService from '../services/PrincipalService';
+import React from "react";
 import { Link } from "react-router-dom";
 
-export default function MapacheBigotonComponent() {
 
-  return (
-    <div className='containerPrincipal'>
-      <div className='im-container' >
-        <div className='form-containerPrincipal'>
-          <div className="col-md-4 d-flex justify-content-center">
-            <img src="imagen/de/mapache.png" />
-          </div>
-          <div className="col-md-8 d-flex flex-column justify-content-center align-items-center">
-            <h1 className="display-4" >Mapache Bigotón</h1>
-            <div className="btn-group mt-4" role="group" aria-label="Menu Principal">
-              <Link to="/" className="btn btn-lg rounded-0" >Inicio</Link>
-              <Link to="/servicios" className="btn btn-lg rounded-0" >Servicios</Link>
-              <Link to="/citas" className="btn btn-lg rounded-0" >Citas</Link>
-              <Link to="/ubicacion" className="btn btn-lg rounded-0" >Ubicación</Link>
-            </div>
-            <div className="mt-4">
-              <Link to="/registro" className="btn btn-lg rounded-pill px-5 me-3" >Registrarse</Link>
-              <Link to="/iniciosesion" className="btn btn-lg rounded-pill px-5" >Iniciar Sesión</Link>
-            </div>
-          </div>
+export default function MapacheBigotonComponent() {
+    return (
+        <div className='containerPrincipal'>
+            <header>
+                <i>Mapache Bigotón</i>
+            </header>
+            <main>
+                <div className='img-container'>
+                <img src='src/mapache.png' alt="Mapache Bigotón" />
+                </div>
+                <div className='form-containerPrincipal'>
+                    <nav>
+                        <ul>
+                            <li><Link to="/">Inicio</Link></li>
+                            <li><Link to="/servicios">Servicios</Link></li>
+                            <li><Link to="/citas">Citas</Link></li>
+                            <li><Link to="/ubicacion">Ubicación</Link></li>
+                        </ul>
+                    </nav>
+                    <div className="buttons">
+                        <Link to="/registro"><button className="register-btn">Registrarse</button></Link>
+                        <Link to="/iniciosesion"><button className="inicio-sesion-btn">Iniciar Sesión</button></Link>
+                    </div>
+                </div>
+            </main>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
