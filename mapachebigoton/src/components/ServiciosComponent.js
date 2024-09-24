09e31bc29c918e99dd1c1a5954ca57308821714d
Belen Hernandez <mary13hernandez2003@gmail.com>
2024-09-24 08:52:04 -0600

    commit actualizacion servicios

--- mapachebigoton/src/components/ServiciosComponent.js
+++ mapachebigoton/src/components/ServiciosComponent.js
@@ -5,31 +5,37 @@ export default function ServiciosComponent() {
     return (
         <div className='containerServicios'>
             <header>
-                <div className="mapache"></div>
+                <div class="mapache">
+                </div>
                 <nav>
                     <ul>
                         <li><a href="/">Inicio</a></li>
-                        <li><a href="/servicios" className="active">Servicios</a></li>
+                        <li><a href="/servicios" class="active">Servicios</a></li>
                         <li><a href="/citas">Citas</a></li>
                         <li><a href="/ubicacion">Ubicación</a></li>
                     </ul>
                 </nav>
-                <button className="cerrar-sesion-btn"><a href="/">Cerrar Sesión</a></button>
+                <button class="cerrar-sesion-btn"><a href="/">Cerrar Sesión</a></button>
             </header>
             <main>
                 <h2>Nuestros Servicios</h2>
-                <div className="servicios">
-                    <div className="servicio">
+                <div class="servicios">
+                    <div class="servicio">
+
                         <p>Corte Escolar</p>
                     </div>
-                    <div className="servicio">
+                    <div class="servicio">
+
                         <p>Corte alineado de bigote</p>
                     </div>
-                    <div className="servicio">
+                    <div class="servicio">
+
                         <p>Tratamiento Facial</p>
                     </div>
                 </div>
             </main>
+
         </div>
+
     );
 }
