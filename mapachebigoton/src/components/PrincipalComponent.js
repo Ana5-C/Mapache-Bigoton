d593118726ac807af69ac0fa5c470b59982db2af
Belen Hernandez <mary13hernandez2003@gmail.com>
2024-09-24 01:05:12 -0600

    commit ubicacion logo
    pagina terminada

--- mapachebigoton/src/components/PrincipalComponent.js
+++ mapachebigoton/src/components/PrincipalComponent.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-
 export default function MapacheBigotonComponent() {
     return (
         <div className='containerPrincipal'>
@@ -9,21 +8,23 @@ export default function MapacheBigotonComponent() {
                 <i>Mapache Bigotón</i>
             </header>
             <main>
-                <div className='img-container'>
-                <img src='imagen/mapache.png' alt="Mapache Bigotón" />
-                </div>
-                <div className='form-containerPrincipal'>
-                    <nav>
-                        <ul>
-                            <li><Link to="/">Inicio</Link></li>
-                            <li><Link to="/servicios">Servicios</Link></li>
-                            <li><Link to="/citas">Citas</Link></li>
-                            <li><Link to="/ubicacion">Ubicación</Link></li>
-                        </ul>
-                    </nav>
-                    <div className="buttons">
-                        <Link to="/registro"><button className="register-btn">Registrarse</button></Link>
-                        <Link to="/iniciosesion"><button className="inicio-sesion-btn">Iniciar Sesión</button></Link>
+                <div className='content'>
+                    <div className='img-container'>
+                        <img src="imagen/mapache.png" alt="Mapache Bigotón" />
+                    </div>
+                    <div className='form-containerPrincipal'>
+                        <nav>
+                            <ul>
+                                <li><Link to="/">Inicio</Link></li>
+                                <li><Link to="/servicios">Servicios</Link></li>
+                                <li><Link to="/citas">Citas</Link></li>
+                                <li><Link to="/ubicacion">Ubicación</Link></li>
+                            </ul>
+                        </nav>
+                        <div className="buttons">
+                            <Link to="/registro"><button className="register-btn">Registrarse</button></Link>
+                            <Link to="/iniciosesion"><button className="inicio-sesion-btn">Iniciar Sesión</button></Link>
+                        </div>
                     </div>
                 </div>
             </main>
--- mapachebigoton/src/components/UbicacionComponent.js
+++ mapachebigoton/src/components/UbicacionComponent.js
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 
 export default function UbicacionComponent() {
     return (
-        <div className='containerPrincipal'>
+        <div className='containerUbicacion'>
             <header>
-                
+                <img src="https://images.app.goo.gl/7KfopC3jCzw2V3B58" alt="Logo" className="logo" />
                 <button className="logout-btn"><Link to="/">Cerrar Sesión</Link></button>
             </header>
             <main>
@@ -18,19 +18,14 @@ export default function UbicacionComponent() {
                             <li><Link to="/ubicacion">Ubicación</Link></li>
                         </ul>
                     </nav>
-                    
                     <i>Barbería El Mapache Bigotón</i>
-
-
                     <div className="info">
                         <p>Nos ubicamos en:</p>
-                            <p>Av. Hidalgo No.118 en Teziutlán, Puebla</p> 
-                            <p>Tel. 1234567890         correo: bar.mapachito@gmail.com</p>
-                        <p></p>
+                        <p>Av. Hidalgo No.118 en Teziutlán, Puebla</p> 
+                        <p>Tel. 1234567890 correo: bar.mapachito@gmail.com</p>
                     </div>
-                
                 </div>
             </main>
         </div>
     );
-    }
+}
