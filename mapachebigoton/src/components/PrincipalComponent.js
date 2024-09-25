971e0dd8bd5a890a50b11385b14b79a15a3aade5
Belen Hernandez <mary13hernandez2003@gmail.com>
2024-09-25 11:45:28 -0600

    commit Principal
    creación de las carpetas componentes, services y la clase principal

---
+++ mapachebigoton/src/components/PrincipalComponent.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function MapacheBigotonComponent() {
+    return (
+        <div className='containerPrincipal'>
+            <header>
+                <i>Mapache Bigotón</i>
+            </header>
+            <main>
+                <div className='content'>
+                    <div className='img-container'>
+                        <img url="mapache.png" alt="Mapache Bigotón" />
+                    </div>
+                    <div className='form-containerPrincipal'>
+                        <nav>
+                            <ul>
+                                <li><Link to="/">Inicio</Link></li>
+                                <li><Link to="/servicios">Servicios</Link></li>
+                                <li><Link to="/citas">Citas</Link></li>
+                                <li><Link to="/ubicacion">Ubicación</Link></li>
+                            </ul>
+                        </nav>
+                        <div className="buttons">
+                            <Link to="/registro"><button className="register-btn">Registrarse</button></Link>
+                            <Link to="/iniciosesion"><button className="inicio-sesion-btn">Iniciar Sesión</button></Link>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </div>
+    );
+}
