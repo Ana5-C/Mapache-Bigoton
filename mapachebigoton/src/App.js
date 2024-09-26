0a0a2c8160ccd2cc831ffb41219889db4632383b
Belen Hernandez <mary13hernandez2003@gmail.com>
2024-09-26 13:07:48 -0600

    commit App.js

--- mapachebigoton/src/App.js
+++ mapachebigoton/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import FootComponent from './components/FootComponent';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import PrincipalComponent from './components/PrincipalComponent';
+import UbicacionComponent from './components/UbicacionComponent';
+import ServiciosComponent from './components/ServiciosComponent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <HeadComponent />
+        <div className='container'>
+          <Routes>
+            <Route path='/'element={<PrincipalComponent/>}></Route>
+            <Route path='/ubicacion' element={<UbicacionComponent/>}></Route>
+            <Route path='/servicios'element={<ServiciosComponent/>}></Route>
+          </Routes>
+        </div>
+        <FootComponent />
+      </BrowserRouter>
     </div>
   );
 }
