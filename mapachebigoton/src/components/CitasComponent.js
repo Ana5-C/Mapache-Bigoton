8afea2bd3f82fcce6ec61e3fb566dafd6039f3a1
Ana Cristina Martinez <am8794041@gmail.com>
2024-09-26 13:10:09 -0600

    commit cambios citas
    cambios de estilos a citas

--- mapachebigoton/src/components/CitasComponent.js
+++ mapachebigoton/src/components/CitasComponent.js
@@ -36,7 +36,7 @@ export const AgendarCitaComponent = () => {
             CitaService.create(cita)
                 .then((response) => {
                     console.log(response.data);
-                    navigate('/');
+                    navigate('/cita');
                 })
                 .catch((error) => {
                     console.log(error);
