2b6a4ca7118d83b0711ace0da1d0fb9c46717049
Belen Hernandez <mary13hernandez2003@gmail.com>
2024-09-28 23:25:51 -0600

    commit Sercicios

--- mapachebigoton/src/components/ServiciosComponent.js
+++ mapachebigoton/src/components/ServiciosComponent.js
@@ -21,33 +21,33 @@ export default function ServiciosComponent() {
                 <h1 className='titleS'>Nuestros Servicios</h1>
                 <div class="servicios">
                     <div class="servicio">
-                        <div className='servicio-containerS' />
-                        <p>Tipo de Servicio: Corte Escolar</p>
-                        <p>Precio: $50.00</p>
+                        <div className='servicio-container1' />
+                        <p>Corte de cabello</p>
+                        <p>Precio: $100.00</p>
                     </div>
                     <div class="servicio">
-                        <div className='servicio-containerS' />
-                        <p>Tipo de Servicio: Corte Escolar</p>
-                        <p>Precio: $50.00</p>
+                        <div className='servicio-container2' />
+                        <p>Afeitado con navaja</p>
+                        <p>Precio: $100.00</p>
                     </div>
                     <div class="servicio">
-                        <div className='servicio-containerS' />
-                        <p>Tipo de Servicio: Corte Escolar</p>
-                        <p>Precio: $50.00</p>
+                        <div className='servicio-container3' />
+                        <p>Arreglo de barba y bigote</p>
+                        <p>Precio: $150.00</p>
                     </div>
                     <div class="servicio">
-                        <div className='servicio-containerS' />
-                        <p>Tipo de Servicio: Corte Escolar</p>
-                        <p>Precio: $50.00</p>
+                        <div className='servicio-container4' />
+                        <p>Tratamientos capilares</p>
+                        <p>Precio: $250.00</p>
                     </div>
                     <div class="servicio">
-                        <div className='servicio-containerS' />
-                        <p>Tipo de Servicio: Corte Escolar</p>
-                        <p>Precio: $50.00</p>
+                        <div className='servicio-container5' />
+                        <p>Tratamiento Facial</p>
+                        <p>Precio: $300.00</p>
                     </div>
                     <div class="servicio">
-                        <div className='servicio-containerS' />
-                        <p>Tipo de Servicio: Corte Escolar</p>
+                        <div className='servicio-container6' />
+                        <p>Corte Escolar</p>
                         <p>Precio: $50.00</p>
                     </div>
                 </div>
