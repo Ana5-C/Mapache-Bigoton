9cd4c4c9e2a021018564527d60daa30506b8fdbd
Belen Hernandez <mary13hernandez2003@gmail.com>
2024-09-30 19:41:02 -0600

    commit Ubicacion

--- mapachebigoton/src/components/UbicacionComponent.js
+++ mapachebigoton/src/components/UbicacionComponent.js
@@ -13,17 +13,17 @@ export default function UbicacionComponent() {
           <Link to="/citas" className='nav-btnU'>Citas</Link>
           <Link to="/ubicacion" className='nav-btn activeU'>Ubicación</Link>
         </nav>
-        <button className='close-btn'><Link to="/">Cerrar Sesion</Link></button>
+        <button className='close-btnU'><Link to="/">Cerrar Sesion</Link></button>
       </header>
 
       {/* Contenido Principal */}
       <main className='content-containerU'>
         <h1 className='titleU'>Barbería El Mapache Bigotón</h1>
         <h2 className='subtitleU'>Nos ubicamos en:</h2>
-        <p className='address'>
+        <p className='addressU'>
           Av. Hidalgo No.118, en Teziutlán, Puebla
         </p>
-        <p className='contact'>
+        <p className='contactU'>
           Tel. 1234567890 &nbsp;&nbsp;&nbsp; Correo: bar.mapachito@gmail.com
         </p>
       </main>
