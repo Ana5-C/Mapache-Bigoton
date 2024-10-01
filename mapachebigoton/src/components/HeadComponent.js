2fb07d24f434bf9b064cc3106883079053ed730a
Ana Cristina Martinez <am8794041@gmail.com>
2024-10-01 00:09:01 -0600

    commit eliminacion
    se eliminaron algunas clases

--- mapachebigoton/src/components/HeadComponent.js
+++ mapachebigoton/src/components/HeadComponent.js
@@ -1,3 +1,10 @@
 export default function HeadComponent(){
-
+    return (
+        <div>
+            <footer className='footer'>
+                <span className='text-muted'>
+                </span>
+            </footer>
+        </div>
+    )
 }
